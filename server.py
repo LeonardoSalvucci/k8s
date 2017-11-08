75e00df31cdb86f5c1aea3b8c0e6f0f4dfa4ff6a
Leonardo Salvucci <le.salvucci@gmail.com>
2017-11-08 13:00:29 -0300

    Versión 3

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world(v2): (%s)" % (socket.gethostname(),))
+        self.write("Hello, world(v3): (%s)" % (socket.gethostname(),))
 
 def make_app():
     return tornado.web.Application([
